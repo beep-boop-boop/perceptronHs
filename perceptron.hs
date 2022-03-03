456913d708ad0f3780eae815d670d40aa6e5062a
beep-boop-boop <ivanbar3330@gmail.com>
2022-03-03 15:21:55 +0000

    Reorganised code structure, fixed sign function

--- perceptron.hs
+++ perceptron.hs
@@ -1,10 +1,8 @@
+{-Utility functions-}
 sign :: (Num a, Ord a, Show a) => a -> a
 sign x | x == 0 = 0
        | x > 0 = 1
-       | x < 0 = -1
-
-loss :: (Num a, Ord a, Fractional a, Show a) => a -> a -> a
-loss x y = if x == y then 0 else 1
+       | otherwise = -1
 
 (/+/) :: (Num a, Ord a, Fractional a, Show a) => [a] -> [a] -> [a] -- vector addition
 (/+/) = zipWith (+)
@@ -15,6 +13,24 @@ xs /*/ a = map (*a) xs
 zeros :: (Num a, Ord a, Fractional a, Show a) => [a] -> [a] -- list of zeros of equal length to the input list
 zeros = map (* 0)
 
+thetasToString :: (Num a, Ord a, Fractional a, Show a) => ([a], a) -> IO() -- starting theta, theta0
+thetasToString (theta, theta0) = putStr ("Theta: \n " ++ foldr (\x acc -> show x ++ "\n" ++ acc) "\n" theta ++ "theta 0: "++ show theta0 ++ "\n")
+{------------------------------------}
+
+{-Sample dataset-}
+x :: (Num a, Ord a, Fractional a) => [[a]]
+x = [[15, 3, 1], [4, 18, 1], [-5,-2, 1], [-1, -10, 1], [-5, 15, 1]]
+y :: (Num a, Ord a, Fractional a) => [a]
+y = [1, -1, -1, 1, -1]
+{---------------------------------------------------------------}
+
+{-Model evaluation-}
+loss :: (Num a, Ord a, Fractional a, Show a) => a -> a -> a
+loss x y = if x == y then 0 else 1
+
+avgLoss :: (Num a, Ord a, Fractional a, Show a) => [a] -> [a] -> a
+avgLoss guesses labels = sum (zipWith loss guesses labels) / fromIntegral (length guesses)
+
 linearClassify :: (Num a, Ord a, Show a) => [a] -> [a] -> a -> a
 linearClassify x theta theta_0 = sign (dotProdTranspose theta x + theta_0) where
     dotProdTranspose xs ys = sum (zipWith (*) xs ys) -- equivalent to xs.T * ys
@@ -23,8 +39,6 @@ linearClassify x theta theta_0 = sign (dotProdTranspose theta x + theta_0) where
 predictions :: (Num a, Ord a, Show a) => [[a]] -> [a] -> a -> [a]
 predictions data_in theta theta0 = [linearClassify x theta theta0 | x <- data_in]   
 
-avgLoss :: (Num a, Ord a, Fractional a, Show a) => [a] -> [a] -> a
-avgLoss guesses labels = sum (zipWith loss guesses labels) / fromIntegral (length guesses)
 
 meanError :: (Num a, Ord a, Fractional a, Show a) => 
     [[a]] -> --data, each sub array is a point
@@ -34,6 +48,11 @@ meanError :: (Num a, Ord a, Fractional a, Show a) =>
 meanError data_in labels (theta, theta0) = avgLoss preds labels where
     preds = [linearClassify x theta theta0 | x <- data_in]
 
+testPerceptron :: (Num a, Ord a, Fractional a, Show a) => [[a]] -> [a] -> a -> a
+testPerceptron data_in labels steps = meanError x y (perceptron data_in labels steps (zeros labels, 0))
+{-------------------------------------------------------------------------------------------------}
+
+{-Perceptron algorithm-}
 perceptronStep :: (Num a, Ord a, Fractional a, Show a) => 
     [[a]] -> -- data
     [a] -> -- labels
@@ -55,16 +74,10 @@ perceptron :: (Num a, Ord a, Fractional a, Show a) =>
 perceptron _ _ 0 (t, t0) = (t, t0)
 perceptron xs ls steps (t, t0) = perceptron xs ls (steps - 1) (perceptronStep xs ls t t0)
 
-
-testPerceptron :: (Num a, Ord a, Fractional a, Show a) => [[a]] -> [a] -> a -> a
-testPerceptron data_in labels steps = meanError x y (perceptron data_in labels steps (zeros labels, 0))
-
-thetasToString :: (Num a, Ord a, Fractional a, Show a) => ([a], a) -> IO() -- starting theta, theta0
-thetasToString (theta, theta0) = putStr ("Theta: \n " ++ foldr (\x acc -> show x ++ "\n" ++ acc) "\n" theta ++ "theta 0: "++ show theta0 ++ "\n")
+{-------------------------------------------------------------------------------------------------------}
 
 
 
-x :: (Num a, Ord a, Fractional a) => [[a]]
-x = [[15, 3, 1], [4, 18, 1], [-5,-2, 1], [-1, -10, 1], [-5, 15, 1]]
-y :: (Num a, Ord a, Fractional a) => [a]
-y = [1, -1, -1, 1, -1]
+
+
+
